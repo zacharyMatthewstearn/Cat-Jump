dae6d85479ad48039f9fba0f8729997a7d3397c5
Zachary Matthewstearn <zammo911@gmail.com>
2016-12-21 11:21:11 -0800

    add touch input for movement and jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -27,22 +27,41 @@ public class PlayerController : MonoBehaviour {
 		animator.SetBool ("Ground", grounded);
 		animator.SetFloat ("vSpeed", rb2d.velocity.y);
 
-		float move = Input.GetAxis ("Horizontal");
-		animator.SetFloat ("Speed", Mathf.Abs (move));
-
-		rb2d.velocity = new Vector2(move * maxSpeed, rb2d.velocity.y);
-		if ((move > 0 && !facingRight) || (move < 0 && facingRight))
-			Flip ();
+//		float move = Input.GetAxis ("Horizontal");
+//		animator.SetFloat ("Speed", Mathf.Abs (move));
+//
+//		rb2d.velocity = new Vector2(move * maxSpeed, rb2d.velocity.y);
+//		if ((move > 0 && !facingRight) || (move < 0 && facingRight))
+//			Flip ();
 	}
 
-	void Update() {
-		if (grounded && Input.GetAxis ("Vertical") > 0) {
+//	void Update() {
+//		if (grounded && Input.GetAxis ("Vertical") > 0) {
+//			animator.SetBool ("Ground", false); // Duplicated for snappy responsiveness
+//			rb2d.AddForce(new Vector2(0, jumpForce));
+//		}
+//	}
+
+	public void Move(int source) {
+		if(source == -1 || source == 1) {
+			rb2d.velocity = new Vector2(source * maxSpeed, rb2d.velocity.y);
+			if ((source > 0 && !facingRight) || (source < 0 && facingRight))
+				Flip ();
+			animator.SetFloat ("Speed", Mathf.Abs (rb2d.velocity.x));
+		}
+		else {
+			Debug.Log ("PLayer's 'Move' method recieved a bad value for 'source'!");
+		}
+	}
+
+	public void Jump() {
+		if(grounded) {
 			animator.SetBool ("Ground", false); // Duplicated for snappy responsiveness
 			rb2d.AddForce(new Vector2(0, jumpForce));
 		}
 	}
 
-	void Flip() {
+	private void Flip() {
 		facingRight = !facingRight;
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
---
+++ Assets/Scripts/TouchInputController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchInputController : MonoBehaviour {
+	PlayerController playerController = null;
+
+	Rect panelLeft;
+	Rect panelRight;
+	Rect panelGesture;
+
+	GameObject M1 = null;
+	GameObject M2 = null;
+	GameObject M3 = null;
+
+
+	void Start () {
+		playerController = GameObject.Find ("Player").GetComponent<PlayerController>();
+
+		panelLeft = GameObject.Find ("Panel_Left").GetComponent<RectTransform> ().rect;
+		panelRight = GameObject.Find ("Panel_Right").GetComponent<RectTransform> ().rect;
+		panelGesture = GameObject.Find ("Panel_Gesture").GetComponent<RectTransform> ().rect;
+
+		panelLeft.position = GameObject.Find ("Panel_Left").transform.position;
+		panelRight.position = GameObject.Find ("Panel_Right").transform.position;
+		panelGesture.position = GameObject.Find ("Panel_Gesture").transform.position;
+
+		M1 = GameObject.Find ("Mushroom_1");
+		M2 = GameObject.Find ("Mushroom_2");
+		M3 = GameObject.Find ("Mushroom_3");
+
+		M1.SetActive (false);
+		M2.SetActive (false);
+		M3.SetActive (false);
+	}
+
+	void Update () {
+		int touchCount = Input.touchCount;
+		if (touchCount > 0)
+		{
+			for(int i = 0; i < touchCount; i++) {
+				Vector2 touchPos = Input.GetTouch(i).position;
+
+				if (panelLeft.Contains (touchPos)) {
+//					M1.SetActive (true);
+					playerController.Move(-1);
+				}
+//				else {
+//					M1.SetActive (false);
+//				}
+
+				if (panelRight.Contains(touchPos)) {
+//					M2.SetActive (true);
+					playerController.Move(1);
+				}
+//				else {
+//					M2.SetActive (false);
+//				}
+
+				if (panelGesture.Contains(touchPos)) {
+//					M3.SetActive (true);
+					playerController.Jump();
+				}
+//				else {
+//					M3.SetActive (false);
+//				}
+			}
+		}
+
+//		if (Input.GetMouseButtonDown(0)) {
+//			Vector3 clickPos = (Input.mousePosition);
+//			if (panelLeft.Contains(clickPos))
+//			{
+//				M1.SetActive (!M1.activeSelf);
+//			}
+//			if (panelRight.Contains(clickPos))
+//			{
+//				M2.SetActive (!M2.activeSelf);
+//			}
+//			if (panelGesture.Contains(clickPos))
+//			{
+//				M3.SetActive (!M3.activeSelf);
+//			}
+//		}
+	}
+
+
+}
