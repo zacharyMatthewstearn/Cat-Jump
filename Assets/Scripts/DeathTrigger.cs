7d51c5f5c2583d2485e30115ad9a260a3bdf9b3a
Zachary Matthewstearn <zammo911@gmail.com>
2016-12-22 09:06:35 -0800

    add deathboxes surrounding level, adjust input panel positioning

---
+++ Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathTrigger : MonoBehaviour {
+	public int sceneToLoad = 3;
+
+	void OnTriggerEnter2D (Collider2D other) {
+		SceneManager.LoadScene (sceneToLoad);
+	}
+}
